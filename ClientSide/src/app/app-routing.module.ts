c51643b43306aec572b0dcc8b41902769fc0b356
Yuval <giyuval0@gmail.com>
2023-07-31 14:55:49 +0300

    started add new page

--- ClientSide/src/app/app-routing.module.ts
+++ ClientSide/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './Components/homepage/homepage.component';
 import { ItempageComponent } from './Components/itempage/itempage.component';
 import { CartPageComponent } from './Components/cart/cart-page/cart-page.component';
+import { NewitemComponent } from './Components/newitem/newitem.component';
 
 const routes: Routes = [
   {path: '', component:HomepageComponent},
   {path: 'details/:id', component:ItempageComponent},
   {path: 'sort/:prop/:order', component:HomepageComponent},
   {path: 'cart', component:CartPageComponent},
+  {path: 'addnew', component:NewitemComponent},
 ];
 
 @NgModule({
